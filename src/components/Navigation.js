3aca674cf9ad6a8b10ff546ffc3f62bdd202ed62
mvrkus <>
2021-08-13 08:32:12 -0700

    Update nav responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,37 +4,39 @@ import { Link } from "gatsby";
 import { RiHome7Fill } from "react-icons/ri";
 import { CartButton } from "./CartButton";
 import { Toast } from "./Toast";
-import { wrapper, left_item, right_item } from "./Navigation.module.scss";
+import { wrapper } from "./Navigation.module.scss";
 
 export const Navigation = () => {
   const { didJustAddToCart } = React.useContext(StoreContext);
 
   return (
     <nav className={`${wrapper} container-xl py-2 `}>
-      <div className={`${left_item} d-flex`}>
-        <Link to="/" className={`nav-link center`}>
-          <RiHome7Fill />
-        </Link>
-        <ul className={`nav`}>
-          <li className="nav-item ">
-            <Link to="/products/women" className="nav-link">
-              Women
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products/men" className="nav-link">
-              Men
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products/kid" className="nav-link">
-              Kids
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className={`${right_item} nav-item d-flex justify-content-end`}>
-        <CartButton />
+      <div className={`w-100 h-100 d-flex justify-content-between `}>
+        <div className={`d-flex`}>
+          <Link to="/" className={`nav-link center`}>
+            <RiHome7Fill />
+          </Link>
+          <ul className={`nav`}>
+            <li className="nav-item center">
+              <Link to="/products/women" className="nav-link">
+                Women
+              </Link>
+            </li>
+            <li className="nav-item center">
+              <Link to="/products/men" className="nav-link">
+                Men
+              </Link>
+            </li>
+            <li className="nav-item center">
+              <Link to="/products/kid" className="nav-link">
+                Kids
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={`nav-item center d-flex justify-content-end`}>
+          <CartButton />
+        </div>
       </div>
       <Toast show={didJustAddToCart} />
     </nav>
