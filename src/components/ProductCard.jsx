c3c871a14be0835acf99469bfcc40ceba0fa9129
mvrkus <>
2021-08-19 14:37:20 -0700

    Fix google fonts import url in custom.scss

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -20,7 +20,7 @@ export const ProductCard = ({ product }) => {
         />
         <div className="card-body">
           <h5 className={`card-title`}>{product.title}</h5>
-          <p className="card-text ">{price}</p>
+          <p className="card-text ">${price}</p>
         </div>
       </Link>
     </div>
